05a2b4bd532db1ee7bff42eb3d4256b89e6f10da
Paweł Mieloch <pawel.mieloch@gmail.com>
2017-04-03 22:27:02 +0200

    separeted schema and data from sql script

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `recipes` VALUES (1,'EASY','Makaron z kurczakiem i szpinakiem',20);
+INSERT INTO `products` VALUES (1,99,0,1,'Pierś z kurczaka',21),(2,371,75,1,'Makaron',13),(3,39,9,0.1,'Cebula',1.1),(4,148,33,0.5,'Czosnek',6),(5,23,3.6,0.4,'Szpinak',2.9),(6,186,3.6,18,'Śmietana',2.5);
+INSERT INTO `ingredients` VALUES (1,400,'GRAM',1),(2,200,'GRAM',2),(3,2,'WHOLE',3),(4,2,'PIECE',4),(5,600,'GRAM',5),(6,300,'GRAM',6);
+INSERT INTO `recipes_ingredients` VALUES (1,1),(1,2),(1,3),(1,4),(1,5),(1,6);
+INSERT INTO `instruction_steps` VALUES (1,'Kurczaka kroimy na niewielkie kawałki i wrzucamy na patelnię. Smażymy przez 2-3 minuty.',1,1),(2,'Następnie dodajemy cebulę oraz czosnek (pokrojone w kostkę). Smażymy 5-6 minut.',2,1),(3,'Dodajemy śmietanę, przyprawy i dusimy 2-3 minuty.',3,1),(4,'Następnie dodajemy umyty szpinak, dusimy 2-3 minuty, dokładnie mieszamy. W międzyczasie gotujemy makaron.',4,1),(5,'Ugotowany makaron łączymy z sosem, dobrze mieszamy.',5,1);
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -35,15 +35,6 @@ CREATE TABLE `ingredients` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `ingredients`
---
-
-LOCK TABLES `ingredients` WRITE;
-/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
-INSERT INTO `ingredients` VALUES (1,400,'GRAM',1),(2,200,'GRAM',2),(3,2,'WHOLE',3),(4,2,'PIECE',4),(5,600,'GRAM',5),(6,300,'GRAM',6);
-/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `instruction_steps`
@@ -63,15 +54,7 @@ CREATE TABLE `instruction_steps` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `instruction_steps`
---
 
-LOCK TABLES `instruction_steps` WRITE;
-/*!40000 ALTER TABLE `instruction_steps` DISABLE KEYS */;
-INSERT INTO `instruction_steps` VALUES (1,'Kurczaka kroimy na niewielkie kawałki i wrzucamy na patelnię. Smażymy przez 2-3 minuty.',1,1),(2,'Następnie dodajemy cebulę oraz czosnek (pokrojone w kostkę). Smażymy 5-6 minut.',2,1),(3,'Dodajemy śmietanę, przyprawy i dusimy 2-3 minuty.',3,1),(4,'Następnie dodajemy umyty szpinak, dusimy 2-3 minuty, dokładnie mieszamy. W międzyczasie gotujemy makaron.',4,1),(5,'Ugotowany makaron łączymy z sosem, dobrze mieszamy.',5,1);
-/*!40000 ALTER TABLE `instruction_steps` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
@@ -91,15 +74,7 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `products`
---
 
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,99,0,1,'Pierś z kurczaka',21),(2,371,75,1,'Makaron',13),(3,39,9,0.1,'Cebula',1.1),(4,148,33,0.5,'Czosnek',6),(5,23,3.6,0.4,'Szpinak',2.9),(6,186,3.6,18,'Śmietana',2.5);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `recipes`
@@ -117,15 +92,6 @@ CREATE TABLE `recipes` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `recipes`
---
-
-LOCK TABLES `recipes` WRITE;
-/*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
-INSERT INTO `recipes` VALUES (1,'EASY','Makaron z kurczakiem i szpinakiem',20);
-/*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `recipes_ingredients`
@@ -143,17 +109,6 @@ CREATE TABLE `recipes_ingredients` (
   CONSTRAINT `FKn58cf1b23kbp00ecui7xtqeiu` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `recipes_ingredients`
---
-
-LOCK TABLES `recipes_ingredients` WRITE;
-/*!40000 ALTER TABLE `recipes_ingredients` DISABLE KEYS */;
-INSERT INTO `recipes_ingredients` VALUES (1,1),(1,2),(1,3),(1,4),(1,5),(1,6);
-/*!40000 ALTER TABLE `recipes_ingredients` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
