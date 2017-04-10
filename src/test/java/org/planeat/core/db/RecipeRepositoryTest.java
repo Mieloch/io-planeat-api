29acacf6795344a17ce73cff4588b88622fd6f4d
Paweł Mieloch <pawel.mieloch@gmail.com>
2017-04-10 23:16:06 +0200

    jenkins

--- src/test/java/org/planeat/core/db/RecipeRepositoryTest.java
+++ src/test/java/org/planeat/core/db/RecipeRepositoryTest.java
@@ -1,17 +1,14 @@
 package org.planeat.core.db;
 
-import org.junit.runner.RunWith;
 import org.planeat.core.model.Recipe;
 import org.planeat.core.model.RecipeBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
 
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
 public class RecipeRepositoryTest {
 
   @Autowired
